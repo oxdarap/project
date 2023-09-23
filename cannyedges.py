beb3c08f4cdb856300827e03c7245267d23f6b88
oxdarap <oxdarap@gmail.com>
2023-09-23 15:50:28 +0300

    canny

---
+++ cannyedges.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+max_lowThreshold = 100
+window_name = 'Edge Map'
+title_trackbar = 'Min Threshold:'
+ratio = 3
+kernel_size = 3
+
+def CannyThreshold(val):
+    low_threshold = val
+    img_blur = cv.blur(src_gray, (3,3))
+    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    mask = detected_edges != 0
+    dst = src * (mask[:,:,None].astype(src.dtype))
+    cv.imshow(window_name, dst)
+
+src = cv.imread('block.png')
+
+if src is None:
+    print('Could not open or find the image')
+    exit(0)
+
+src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+cv.namedWindow(window_name)
+cv.createTrackbar(title_trackbar, window_name, 0, max_lowThreshold, CannyThreshold)
+CannyThreshold(0)
+cv.waitKey()
