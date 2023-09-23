033a012d99bb58b42f9406e5d3be70e120014823
oxdarap <oxdarap@gmail.com>
2023-09-23 15:31:17 +0300

    laplace

---
+++ laplace.py
@@ -0,0 +1,35 @@
+import sys
+import cv2 as cv
+import numpy as np
+
+def main(argv):
+
+    kernel_size = 3
+    window_name = "Laplace Demo"
+
+    imageName = argv[0] if len(argv) > 0 else 'block.png'
+    src = cv.imread(cv.samples.findFile(imageName), cv.IMREAD_COLOR)
+
+    if src is None:
+        print('Error opening image')
+        print('Program Arguments: [image_name -- default block.png]')
+        return -1
+
+    src = cv.GaussianBlur(src, (3, 3), 0)
+
+    src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+    cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
+
+    dst = cv.Laplacian(src_gray, cv.CV_16S, ksize=kernel_size)
+
+    abs_dst = cv.convertScaleAbs(dst, alpha=2.0, beta=0)
+
+    # Display the result
+    cv.imshow(window_name, abs_dst)
+    cv.waitKey(0)
+
+    return 0
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
